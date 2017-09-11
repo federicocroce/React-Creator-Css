97f0917407fabb6d84589d6b1b56aa88b37286f6
Federico Croce <fede.croce.123@gmail.com>
2017-09-11 09:25:07 -0300

    Aregrado de la herramienta de seguimiento de redux

--- src/Components/Config/Store.js
+++ src/Components/Config/Store.js
@@ -1,7 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { routerMiddleware } from "react-router-redux";
 import createHistory from 'history/createBrowserHistory';
 import { batchedSubscribe } from 'redux-batched-subscribe';
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import { batchedUpdatesMiddleware } from 'redux-batched-updates';
 // import thunk from 'redux-thunk'
 
@@ -21,7 +22,7 @@ const middleware = routerMiddleware(history);
 
 const store = createStore(
     reducer,
-    compose(
+    composeWithDevTools(
         applyMiddleware(middleware)
     )
 )
