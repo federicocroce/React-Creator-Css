78e2fc8527387f5a2d9b053d4d0d2536f3a2f2e8
Federico Croce <federico.croce@bbva.com>
2017-10-06 19:52:03 -0300

    Agregado de la librerio Axios para llamados http

--- src/Components/Sections/Home.js
+++ src/Components/Sections/Home.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 class Home extends React.Component {
 
-  render() {
+  constructor(){
+    super();
 
+    this.state = {
+      names: []
+    }
+
+    axios.get("http://localhost:3001/names")
+      .then(response => {
+        this.setState({
+          names: response.data
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+
+
+  render() {
     console.log("Home");
     return (
       <div>
-        <h1>
-          Esta es la HOME
-        </h1>
+        <h1>Esta es la HOME</h1>
+        <ul>
+          {this.state.names.map( (name, index) =>
+            <li key={index}>{name}</li>
+          )}
+        </ul>
         <React.components.Button className='primary-button' label='Volver' back />
       </div>
     );
@@ -19,3 +42,4 @@ class Home extends React.Component {
 }
 
 export default Home;
+
